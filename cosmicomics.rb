5d51c875cc36f1c7539eaa9c831cc2b3d8442b31
Justin Bean <beanjustinr@gmail.com>
2015-09-30 15:33:29 -0400

    Singled out 624 anagram arrays

--- cosmicomics.rb
+++ cosmicomics.rb
@@ -3,6 +3,22 @@ require "pry"
 words = File.read('words.txt').split("\n")
 
 
-words.map! do |word|
-  {word => word.chars.sort}
+# word_hash = words.map! do |word|
+#   {word => word.chars.sort.join.eql?}
+#   end
+
+check_anagrams = words.group_by { |element| element.chars.sort }.values
+
+
+anagrams = []
+check_anagrams.each do |word|
+  if word.length > 1
+    anagrams.push word
   end
+end
+
+
+
+#624 total anagrams
+
+puts counter
